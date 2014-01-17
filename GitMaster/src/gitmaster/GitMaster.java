dfdc4563a971f54964c7e6a5f8a756ac57bb471a
B482384 <B482384@U0F02565.net.in.iantel.com.uy>
2014-01-17 13:25:22 -0200

    Primer commit

---
+++ GitMaster/src/gitmaster/GitMaster.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitmaster;
+
+/**
+ *
+ * @author B482384
+ */
+public class GitMaster {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
