fb3ab70920a0e388d2a4f1abe6e5c3be39acc051
B482384 <B482384@U0F02565.net.in.iantel.com.uy>
2014-01-17 13:28:09 -0200

    segundo commit

--- GitMaster/src/gitmaster/GitMaster.java
+++ GitMaster/src/gitmaster/GitMaster.java
@@ -14,6 +14,8 @@ public class GitMaster {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String j = "new world";
+           
         // TODO code application logic here
     }
 }
